4dfcd7f6ebeb364d64009a7bf8d4fbefac63eee8
shosholanda <davidshiro.pichu@ciencias.unam.mx>
2022-12-08 22:11:01 -0600

    Se terminaron los casos de uso UPDATE y DELETE de reservación. También se hicieron algunas pruebas y optimizaciones en cuanto a el formulario y se agregó también los botones de editar y eliminar para cada publicacion. También se agregaron errores cuando no se contesta bien el formulario de tipo flash

--- models/model_reservaciones.py
+++ models/model_reservaciones.py
@@ -5,15 +5,20 @@ from alchemyClasses.__init__ import db
 #Aquí estan todas las consultas a la base de datos
 
 #Obtiene una reservacion
-def obtener_reservacion(email):
-    ans = Reservacion.query.filter(Reservacion.correo == email).first()
+def obtener_reservacion(idreservacion):
+    ans = Reservacion.query.filter(Reservacion.idreservacion == idreservacion).first()
     return ans
 
 #Obtiene todas las reservaciones de la persona
 def obtener_reservaciones(email):
     return Reservacion.query.filter(Reservacion.correo == email)
 
-#Crea una reservacion
+#Crea una reservacion. También sirve para actualizar
 def crear_reservacion(reservacion):
     db.session.add(reservacion)
+    db.session.commit()
+
+#Elimina una reservacion
+def elimina_reservacion(reservacion):
+    db.session.delete(reservacion)
     db.session.commit()
